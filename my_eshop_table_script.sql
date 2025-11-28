fea9dd31874ef3c45bb55c514745446f4661ad4f
SilviaBalmaseda <davsil888@gmail.com>
2025-11-28 20:25:33 +0100

    Cambios en el script
    Modificar los not null por default y poner INTEGER

--- my_eshop_table_script.sql
+++ my_eshop_table_script.sql
@@ -20,32 +20,33 @@ SET FOREIGN_KEY_CHECKS=1;
 -- CLIENTE
 CREATE TABLE IF NOT EXISTS cliente (
   nif_cif varchar(20) NOT NULL,
-  nombre_completo varchar(100) NOT NULL,
-  email varchar(150) NOT NULL,
-  fecha_registro datetime NOT NULL,
+  nombre_completo varchar(100) DEFAULT '',
+  email varchar(150) DEFAULT '',
+  fecha_registro datetime DEFAULT NULL,
   PRIMARY KEY (nif_cif),
   UNIQUE KEY NewTable_UNIQUE (email)
 ) ENGINE=InnoDB;
 
 
--- INFORMACION_FISCAL (Relación 1:1 con Cliente)
+-- INFORMACION_FISCAL (Relación 1:1 con cliente)
 CREATE TABLE IF NOT EXISTS informacion_fiscal (
   nif_cif varchar(20) NOT NULL,
-  telefono varchar(20) NOT NULL,
-  direccion_fiscal varchar(255) NOT NULL,
+  telefono varchar(20) DEFAULT '',
+  direccion_fiscal varchar(255) DEFAULT '',
   PRIMARY KEY (nif_cif),
   CONSTRAINT informacion_fiscal_cliente_FK 
   FOREIGN KEY (nif_cif) REFERENCES cliente (nif_cif) 
   		ON UPDATE CASCADE
+  		ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
 
--- COMPRA 
+-- COMPRA (Relación N:1 con cliente)
 CREATE TABLE IF NOT EXISTS compra (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  fecha_compra datetime NOT NULL,
-  estado varchar(20) NOT NULL,
-  direccion_entrega varchar(255) NOT NULL,
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  fecha_compra datetime DEFAULT NULL,
+  estado varchar(20) DEFAULT '',
+  direccion_entrega varchar(255) DEFAULT '',
   precio_total decimal(10,2) NOT NULL,
   cliente_nif_cif varchar(20) NOT NULL,
   PRIMARY KEY (id),
@@ -58,20 +59,20 @@ CREATE TABLE IF NOT EXISTS compra (
 
 -- ARTICULO
 CREATE TABLE IF NOT EXISTS articulo (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  nombre varchar(100) NOT NULL,
-  descripcion text DEFAULT NULL,
-  precio_actual decimal(10,2) NOT NULL,
-  stock int(11) NOT NULL,
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  nombre varchar(100) DEFAULT '',
+  descripcion text DEFAULT '',
+  precio_actual decimal(10,2) DEFAULT 0.0,
+  stock INTEGER 0,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
--- ARTICULO_COMPRA
+-- ARTICULO_COMPRA (Relación N:N entre articulo y compra)
 CREATE TABLE IF NOT EXISTS articulo_compra (
-  articulo_id int(11) NOT NULL,
-  compra_id int(11) NOT NULL,
-  unidades int(11) NOT NULL,
-  precio_compra decimal(10,2) NOT NULL,
+  articulo_id INTEGER NOT NULL,
+  compra_id INTEGER NOT NULL,
+  unidades INTEGER DEFAULT 0,
+  precio_compra decimal(10,2) DEFAULT 0.0,
   PRIMARY KEY (articulo_id,compra_id),
   KEY articulo_compra_compra_FK (compra_id),
   CONSTRAINT articulo_compra_articulo_FK 
